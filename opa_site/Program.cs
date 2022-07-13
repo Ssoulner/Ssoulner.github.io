a1dc903b861b0f524f91c189f00f66cf9e76aa37
Tjipke-Feenstra <tjipke.feenstra@gmail.com>
2022-07-13 13:22:53 +0100

    keywords toegevoegd

--- opa_site/Program.cs
+++ opa_site/Program.cs
@@ -12,7 +12,7 @@ namespace opa_site
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
-
+            
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
             await builder.Build().RunAsync();
